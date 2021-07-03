5d796eb959173acccdc0cd5fcbdbab5fd63b0efa
kinshyn-stas <kinshyn.stas.ea@gmail.com>
2021-07-03 19:35:07 +0300

    fix loader

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,7 +1,11 @@
 export default function Loading(props){
     return (
         <div className="loading">
-            Loading...
+            <svg width="50px" height="50px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+				<circle cx="50" cy="50" r="32" strokeWidth="8" stroke="#000000" strokeDasharray="50.26548245743669 50.26548245743669" fill="none" strokeLinecap="round">
+					<animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" keyTimes="0;1" values="0 50 50;360 50 50"></animateTransform>
+				</circle>
+			</svg>
         </div>
     )
 }
